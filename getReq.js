7f8e809f1bdbed6a18b3a83b07aab73df15e755d
Nikolai <nikolaigauer@gmail.com>
2017-07-04 00:01:39 +0000

    Adding this get request, which seems to work since it downloaded the jpg

---
+++ getReq.js
@@ -0,0 +1,14 @@
+var request = require('request');
+var fs = require('fs');
+
+request.get('https://sytantris.github.io/http-examples/future.jpg')               // Note 1
+       .on('error', function (err) {
+       console.log("ERROR ERROR ERROR!!!! OH DEAR ME, WE SEEM TO HAVE AN ERROR")                                // Note 2
+         throw err;
+       })
+       .on('response', function (response) {                           // Note 3
+         console.log('Response Status Code: ', response.statusCode, 'CHECK THE STATUS: ', response.statusMessage, response.headers['THIS IS THE CONTENT TYPE!'])
+         // console.log('CHECK THE STATUS: ', response.statusMessage, )
+         // console.log(response.headers['content-type'])
+       })
+       .pipe(fs.createWriteStream('./future.jpg'));
